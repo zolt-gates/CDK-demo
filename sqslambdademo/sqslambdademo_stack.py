ebb8d8a5b24358c3a93c28ef51f8bc7e48129e82
souljay101 <soji_oyetayo@yahoo.com>
2023-09-06 10:49:57 +0100

    aws cdk stack demo with sqs and lambda

--- sqslambdademo/sqslambdademo_stack.py
+++ sqslambdademo/sqslambdademo_stack.py
@@ -2,10 +2,10 @@ from constructs import Construct
 from aws_cdk import (
     Duration,
     Stack,
-    aws_iam as iam,
     aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as lambda_,
+    aws_lambda_event_sources as lambda_event_sources,
+    
 )
 
 
@@ -13,14 +13,21 @@ class SqslambdademoStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
+        # create our queue
         queue = sqs.Queue(
             self, "SqslambdademoQueue",
             visibility_timeout=Duration.seconds(300),
         )
+        # create our lambda function
+        sqs_lambda = lambda_.Function(self, "SQSLambda",
+                                      handler='lambda_handler.handler',
+                                      runtime=lambda_.Runtime.PYTHON_3_11,
+                                      code=lambda_.Code.from_asset("sqslambdademo/lambda")
+                                      )
+        # create our event source
+        sqs_event_source = lambda_event_sources.SqsEventSource(queue)
 
-        topic = sns.Topic(
-            self, "SqslambdademoTopic"
-        )
+        # add the event source to lambda
+        sqs_lambda.add_event_source(sqs_event_source)
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        
